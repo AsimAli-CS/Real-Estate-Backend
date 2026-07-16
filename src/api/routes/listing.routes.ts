26e4b472010a78882d208029462e7f1b7fdc976b
Asim Ali <asimaliii1121@gmail.com>
2026-07-16 08:49:08 +0500

    listing

--- src/api/routes/listing.routes.ts
+++ src/api/routes/listing.routes.ts
@@ -5,9 +5,9 @@ import listingController from '../controllers/user/listing.controller';
 const router = Router();
 
 router.post('/createlisting',authorize.validateAuth, listingController.createListing);
-router.put('/updatelisting',authorize.validateAuth, listingController.createListing);
-router.get('/getlistingbyid/:id',authorize.validateAuth, listingController.createListing);
-router.get('/getlistings',authorize.validateAuth, listingController.createListing);
-router.delete('/deletelistingbyid/:id',authorize.validateAuth, listingController.createListing);
+router.put('/updatelisting',authorize.validateAuth, listingController.updateListing);
+router.get('/getlistingbyid/:id',authorize.validateAuth, listingController.getListingById);
+router.get('/getlistings',authorize.validateAuth, listingController.getListings);
+router.delete('/deletelistingbyid/:id',authorize.validateAuth, listingController.deleteListingById);
 
 export default router;
